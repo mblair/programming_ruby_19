08a620e5b8554cd660c062b2e3731801ed5c472c
Matt Blair <me@matthewblair.net>
2011-04-14 18:02:54 -0400

    cosmetic change

--- ch03/classes_working_with_other_classes/stock_stats.rb
+++ ch03/classes_working_with_other_classes/stock_stats.rb
@@ -3,6 +3,6 @@ reader = CsvReader.new
 ARGV.each do |csv_file_name|
   STDERR.puts "Processing #{csv_file_name}"
   reader.read_in_csv_data(csv_file_name)
-  end
+end
 
 puts "Total value = #{reader.total_value_in_stock}"
